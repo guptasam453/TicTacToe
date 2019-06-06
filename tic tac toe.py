366f2db4b34651d86642154f96d04b31c1682207
guptasam453 <46249126+guptasam453@users.noreply.github.com>
2019-06-06 23:46:49 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,5 +1,6 @@
 class TicTacToe:
 
+    # Designing a new board with user defined size
     def make_board(self, dim):
         board = [];
         for i in range(dim):
@@ -9,16 +10,19 @@ class TicTacToe:
             board.append(row)    
         return board
     
+    # Initializing the object with user defined dimension and board
     def __init__(self, dim):
         self.board = self.make_board(dim)
         self.dim = dim
     
+    # Display the current situation of the board
     def display_board(self):
         for i in range(self.dim):
             for j in range(self.dim):
                 print(self.board[i][j] + "  ", end="")
             print("\n")
     
+    # To play turn of both the users one by one
     def play_game(self):
         self.display_board()
         
@@ -34,12 +38,14 @@ class TicTacToe:
         y = int(input("Player1 Enter Col Value: "))
         val = input("Player1 Enter Value: ")
         
+        # till user does not enter the correct position of the turn the loop goes on
         while(self.handle_turn(x, y, val) == -1):
             x = int(input("Player1 Enter Row Value: "))
             y = int(input("Player1 Enter Col Value: "))
             val = input("Player1 Enter Value: ")
         
         
+        # check the status of the board after player1 turn
         status = self.check_status(x, y, val)
         
         if status == 0:
@@ -53,6 +59,7 @@ class TicTacToe:
             return
         
         
+        # same things with player2
         self.display_board()
         
         check = input("Player2 Do You Want To Quit: ")
@@ -86,6 +93,7 @@ class TicTacToe:
         
         return
         
+    # to alter the board after correct turn
     def handle_turn(self, x, y, val):
         if(self.board[x][y]!="-"):
             print("Invalid Entry")
@@ -94,6 +102,8 @@ class TicTacToe:
         self.board[x][y] = val
         return
     
+    # to check the win or deuce situation after each turn
+    # and to clear the board of the same object if any of these 2 occurs
     def check_status(self, x, y, val):
 
         count = 0
